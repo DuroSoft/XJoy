f1ff1a58c7792b7cb1f19f9cf10b99fcd7eacf00
Sam Johnson <sam@durosoft.com>
2019-07-17 13:04:14 -0400

    fix coding style

--- XJoy/XJoy.cpp
+++ XJoy/XJoy.cpp
@@ -97,65 +97,65 @@ enum JOYCON_BUTTON {
 std::unordered_map<JOYCON_BUTTON, XUSB_BUTTON> button_mappings;
 
 std::tuple<JOYCON_REGION, JOYCON_BUTTON> string_to_joycon_button(std::string input) {
-  if (input == "L_DPAD_LEFT") return std::make_tuple(LEFT_DPAD, L_DPAD_LEFT);
-  if (input == "L_DPAD_DOWN") return std::make_tuple(LEFT_DPAD, L_DPAD_DOWN);
-  if (input == "L_DPAD_UP") return std::make_tuple(LEFT_DPAD, L_DPAD_UP);
-  if (input == "L_DPAD_RIGHT") return std::make_tuple(LEFT_DPAD, L_DPAD_RIGHT);
-  if (input == "L_DPAD_SL") return std::make_tuple(LEFT_DPAD, L_DPAD_SL);
-  if (input == "L_DPAD_SR") return std::make_tuple(LEFT_DPAD, L_DPAD_SR);
-  if (input == "L_ANALOG_LEFT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_LEFT);
-  if (input == "L_ANALOG_UP_LEFT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_UP_LEFT);
-  if (input == "L_ANALOG_UP") return std::make_tuple(LEFT_ANALOG, L_ANALOG_UP);
-  if (input == "L_ANALOG_UP_RIGHT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_UP_RIGHT);
-  if (input == "L_ANALOG_RIGHT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_RIGHT);
-  if (input == "L_ANALOG_DOWN_RIGHT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_DOWN_RIGHT);
-  if (input == "L_ANALOG_DOWN") return std::make_tuple(LEFT_ANALOG, L_ANALOG_DOWN);
-  if (input == "L_ANALOG_DOWN_LEFT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_DOWN_LEFT);
-  if (input == "L_ANALOG_NONE") return std::make_tuple(LEFT_ANALOG, L_ANALOG_NONE);
-  if (input == "L_SHOULDER") return std::make_tuple(LEFT_AUX, L_SHOULDER);
-  if (input == "L_TRIGGER") return std::make_tuple(LEFT_AUX, L_TRIGGER);
-  if (input == "L_CAPTURE") return std::make_tuple(LEFT_AUX, L_CAPTURE);
-  if (input == "L_MINUS") return std::make_tuple(LEFT_AUX, L_MINUS);
-  if (input == "L_STICK") return std::make_tuple(LEFT_AUX, L_STICK);
-  if (input == "R_BUT_A") return std::make_tuple(RIGHT_BUTTONS, R_BUT_A);
-  if (input == "R_BUT_B") return std::make_tuple(RIGHT_BUTTONS, R_BUT_B);
-  if (input == "R_BUT_Y") return std::make_tuple(RIGHT_BUTTONS, R_BUT_Y);
-  if (input == "R_BUT_X") return std::make_tuple(RIGHT_BUTTONS, R_BUT_X);
-  if (input == "R_BUT_SL") return std::make_tuple(RIGHT_BUTTONS, R_BUT_SL);
-  if (input == "R_BUT_SR") return std::make_tuple(RIGHT_BUTTONS, R_BUT_SR);
-  if (input == "R_SHOULDER") return std::make_tuple(RIGHT_AUX, R_SHOULDER);
-  if (input == "R_TRIGGER") return std::make_tuple(RIGHT_AUX, R_TRIGGER);
-  if (input == "R_HOME") return std::make_tuple(RIGHT_AUX, R_HOME);
-  if (input == "R_PLUS") return std::make_tuple(RIGHT_AUX, R_PLUS);
-  if (input == "R_STICK") return std::make_tuple(RIGHT_AUX, R_STICK);
-  if (input == "R_ANALOG_LEFT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_LEFT);
-  if (input == "R_ANALOG_UP_LEFT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_UP_LEFT);
-  if (input == "R_ANALOG_UP") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_UP);
-  if (input == "R_ANALOG_UP_RIGHT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_UP_RIGHT);
-  if (input == "R_ANALOG_RIGHT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_RIGHT);
-  if (input == "R_ANALOG_DOWN_RIGHT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_DOWN_RIGHT);
-  if (input == "R_ANALOG_DOWN") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_DOWN);
-  if (input == "R_ANALOG_DOWN_LEFT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_DOWN_LEFT);
-  if (input == "R_ANALOG_NONE") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_NONE);
+  if(input == "L_DPAD_LEFT") return std::make_tuple(LEFT_DPAD, L_DPAD_LEFT);
+  if(input == "L_DPAD_DOWN") return std::make_tuple(LEFT_DPAD, L_DPAD_DOWN);
+  if(input == "L_DPAD_UP") return std::make_tuple(LEFT_DPAD, L_DPAD_UP);
+  if(input == "L_DPAD_RIGHT") return std::make_tuple(LEFT_DPAD, L_DPAD_RIGHT);
+  if(input == "L_DPAD_SL") return std::make_tuple(LEFT_DPAD, L_DPAD_SL);
+  if(input == "L_DPAD_SR") return std::make_tuple(LEFT_DPAD, L_DPAD_SR);
+  if(input == "L_ANALOG_LEFT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_LEFT);
+  if(input == "L_ANALOG_UP_LEFT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_UP_LEFT);
+  if(input == "L_ANALOG_UP") return std::make_tuple(LEFT_ANALOG, L_ANALOG_UP);
+  if(input == "L_ANALOG_UP_RIGHT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_UP_RIGHT);
+  if(input == "L_ANALOG_RIGHT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_RIGHT);
+  if(input == "L_ANALOG_DOWN_RIGHT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_DOWN_RIGHT);
+  if(input == "L_ANALOG_DOWN") return std::make_tuple(LEFT_ANALOG, L_ANALOG_DOWN);
+  if(input == "L_ANALOG_DOWN_LEFT") return std::make_tuple(LEFT_ANALOG, L_ANALOG_DOWN_LEFT);
+  if(input == "L_ANALOG_NONE") return std::make_tuple(LEFT_ANALOG, L_ANALOG_NONE);
+  if(input == "L_SHOULDER") return std::make_tuple(LEFT_AUX, L_SHOULDER);
+  if(input == "L_TRIGGER") return std::make_tuple(LEFT_AUX, L_TRIGGER);
+  if(input == "L_CAPTURE") return std::make_tuple(LEFT_AUX, L_CAPTURE);
+  if(input == "L_MINUS") return std::make_tuple(LEFT_AUX, L_MINUS);
+  if(input == "L_STICK") return std::make_tuple(LEFT_AUX, L_STICK);
+  if(input == "R_BUT_A") return std::make_tuple(RIGHT_BUTTONS, R_BUT_A);
+  if(input == "R_BUT_B") return std::make_tuple(RIGHT_BUTTONS, R_BUT_B);
+  if(input == "R_BUT_Y") return std::make_tuple(RIGHT_BUTTONS, R_BUT_Y);
+  if(input == "R_BUT_X") return std::make_tuple(RIGHT_BUTTONS, R_BUT_X);
+  if(input == "R_BUT_SL") return std::make_tuple(RIGHT_BUTTONS, R_BUT_SL);
+  if(input == "R_BUT_SR") return std::make_tuple(RIGHT_BUTTONS, R_BUT_SR);
+  if(input == "R_SHOULDER") return std::make_tuple(RIGHT_AUX, R_SHOULDER);
+  if(input == "R_TRIGGER") return std::make_tuple(RIGHT_AUX, R_TRIGGER);
+  if(input == "R_HOME") return std::make_tuple(RIGHT_AUX, R_HOME);
+  if(input == "R_PLUS") return std::make_tuple(RIGHT_AUX, R_PLUS);
+  if(input == "R_STICK") return std::make_tuple(RIGHT_AUX, R_STICK);
+  if(input == "R_ANALOG_LEFT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_LEFT);
+  if(input == "R_ANALOG_UP_LEFT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_UP_LEFT);
+  if(input == "R_ANALOG_UP") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_UP);
+  if(input == "R_ANALOG_UP_RIGHT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_UP_RIGHT);
+  if(input == "R_ANALOG_RIGHT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_RIGHT);
+  if(input == "R_ANALOG_DOWN_RIGHT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_DOWN_RIGHT);
+  if(input == "R_ANALOG_DOWN") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_DOWN);
+  if(input == "R_ANALOG_DOWN_LEFT") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_DOWN_LEFT);
+  if(input == "R_ANALOG_NONE") return std::make_tuple(RIGHT_ANALOG, R_ANALOG_NONE);
   throw "invalid joycon button: " + input;
 }
 
 XUSB_BUTTON string_to_xbox_button(std::string input) {
-  if (input == "XUSB_GAMEPAD_DPAD_UP") return XUSB_GAMEPAD_DPAD_UP;
-  if (input == "XUSB_GAMEPAD_DPAD_DOWN") return XUSB_GAMEPAD_DPAD_DOWN;
-  if (input == "XUSB_GAMEPAD_DPAD_LEFT") return XUSB_GAMEPAD_DPAD_LEFT;
-  if (input == "XUSB_GAMEPAD_DPAD_RIGHT") return XUSB_GAMEPAD_DPAD_RIGHT;
-  if (input == "XUSB_GAMEPAD_START") return XUSB_GAMEPAD_START;
-  if (input == "XUSB_GAMEPAD_BACK") return XUSB_GAMEPAD_BACK;
-  if (input == "XUSB_GAMEPAD_LEFT_THUMB") return XUSB_GAMEPAD_LEFT_THUMB;
-  if (input == "XUSB_GAMEPAD_RIGHT_THUMB") return XUSB_GAMEPAD_RIGHT_THUMB;
-  if (input == "XUSB_GAMEPAD_LEFT_SHOULDER") return XUSB_GAMEPAD_LEFT_SHOULDER;
-  if (input == "XUSB_GAMEPAD_RIGHT_SHOULDER") return XUSB_GAMEPAD_RIGHT_SHOULDER;
-  if (input == "XUSB_GAMEPAD_GUIDE") return XUSB_GAMEPAD_GUIDE;
-  if (input == "XUSB_GAMEPAD_A") return XUSB_GAMEPAD_A;
-  if (input == "XUSB_GAMEPAD_B") return XUSB_GAMEPAD_B;
-  if (input == "XUSB_GAMEPAD_X") return XUSB_GAMEPAD_X;
-  if (input == "XUSB_GAMEPAD_Y") return XUSB_GAMEPAD_Y;
+  if(input == "XUSB_GAMEPAD_DPAD_UP") return XUSB_GAMEPAD_DPAD_UP;
+  if(input == "XUSB_GAMEPAD_DPAD_DOWN") return XUSB_GAMEPAD_DPAD_DOWN;
+  if(input == "XUSB_GAMEPAD_DPAD_LEFT") return XUSB_GAMEPAD_DPAD_LEFT;
+  if(input == "XUSB_GAMEPAD_DPAD_RIGHT") return XUSB_GAMEPAD_DPAD_RIGHT;
+  if(input == "XUSB_GAMEPAD_START") return XUSB_GAMEPAD_START;
+  if(input == "XUSB_GAMEPAD_BACK") return XUSB_GAMEPAD_BACK;
+  if(input == "XUSB_GAMEPAD_LEFT_THUMB") return XUSB_GAMEPAD_LEFT_THUMB;
+  if(input == "XUSB_GAMEPAD_RIGHT_THUMB") return XUSB_GAMEPAD_RIGHT_THUMB;
+  if(input == "XUSB_GAMEPAD_LEFT_SHOULDER") return XUSB_GAMEPAD_LEFT_SHOULDER;
+  if(input == "XUSB_GAMEPAD_RIGHT_SHOULDER") return XUSB_GAMEPAD_RIGHT_SHOULDER;
+  if(input == "XUSB_GAMEPAD_GUIDE") return XUSB_GAMEPAD_GUIDE;
+  if(input == "XUSB_GAMEPAD_A") return XUSB_GAMEPAD_A;
+  if(input == "XUSB_GAMEPAD_B") return XUSB_GAMEPAD_B;
+  if(input == "XUSB_GAMEPAD_X") return XUSB_GAMEPAD_X;
+  if(input == "XUSB_GAMEPAD_Y") return XUSB_GAMEPAD_Y;
   throw "invalid xbox button: " + input;
 }
 
@@ -244,104 +244,97 @@ std::string vigem_error_to_string(VIGEM_ERROR err) {
   }
 }
 
-
-void subcomm(hid_device* joycon, u8* in, u8 len, u8 comm, u8 get_response, u8 is_left)
-{
-	u8 buf[OUT_BUFFER_SIZE] = { 0 };
-	buf[0] = 0x1;
-	buf[1] = global_counter[is_left];
-	buf[2] = 0x0;
-	buf[3] = 0x1;
-	buf[4] = 0x40;
-	buf[5] = 0x40;
-	buf[6] = 0x0;
-	buf[7] = 0x1;
-	buf[8] = 0x40;
-	buf[9] = 0x40;
-	buf[10] = comm;
-	for (int i = 0; i < len; ++i) {
-		buf[11 + i] = in[i];
-	}
-  if (global_counter[is_left] == 0xf) global_counter[is_left] = 0;
+void subcomm(hid_device* joycon, u8* in, u8 len, u8 comm, u8 get_response, u8 is_left) {
+  u8 buf[OUT_BUFFER_SIZE] = { 0 };
+  buf[0] = 0x1;
+  buf[1] = global_counter[is_left];
+  buf[2] = 0x0;
+  buf[3] = 0x1;
+  buf[4] = 0x40;
+  buf[5] = 0x40;
+  buf[6] = 0x0;
+  buf[7] = 0x1;
+  buf[8] = 0x40;
+  buf[9] = 0x40;
+  buf[10] = comm;
+  for(int i = 0; i < len; ++i) {
+    buf[11 + i] = in[i];
+  }
+  if(global_counter[is_left] == 0xf) global_counter[is_left] = 0;
   else ++global_counter[is_left];
-	//for (int i = 0; i < 15; ++i) {
-	//	printf("%x ", buf[i]);
-	//}
-	//printf("\n");
-	hid_write(joycon, buf, OUT_BUFFER_SIZE);
-	if (get_response) {
-		int n = hid_read_timeout(joycon, data, DATA_BUFFER_SIZE, 50);
+  //for(int i = 0; i < 15; ++i) {
+  //  printf("%x ", buf[i]);
+  //}
+  //printf("\n");
+  hid_write(joycon, buf, OUT_BUFFER_SIZE);
+  if(get_response) {
+    int n = hid_read_timeout(joycon, data, DATA_BUFFER_SIZE, 50);
 
-		/*printf("response: ");
-		for (int i = 0; i < 35; ++i) {
-			printf("%x ", data[i]);
-		}
-		printf("\n");
+    /*printf("response: ");
+    for(int i = 0; i < 35; ++i) {
+      printf("%x ", data[i]);
+    }
+    printf("\n");
 
-		if (data[14] != comm) {
-			printf("subcomm return fail\n");
-		}
-		else printf("subcomm return correct\n");
-		*/
-	}
+    if(data[14] != comm) {
+      printf("subcomm return fail\n");
+    }
+    else printf("subcomm return correct\n");
+    */
+  }
 }
 
-u8* read_spi(hid_device *jc, u8 addr1, u8 addr2, int len, u8 is_left)
-{
-	u8 buf[] = { addr2, addr1, 0x00, 0x00, (u8)len };
-	int tries = 0;
-	do {
-		++tries;
-		subcomm(jc, buf, 5, 0x10, 1, is_left);
-	} while (tries < 10 && !(data[15] == addr2 && data[16] == addr1));
-	return data + 20;
+u8* read_spi(hid_device *jc, u8 addr1, u8 addr2, int len, u8 is_left) {
+  u8 buf[] = { addr2, addr1, 0x00, 0x00, (u8)len };
+  int tries = 0;
+  do {
+    ++tries;
+    subcomm(jc, buf, 5, 0x10, 1, is_left);
+  } while (tries < 10 && !(data[15] == addr2 && data[16] == addr1));
+  return data + 20;
 }
 
-void get_stick_cal(hid_device* jc, u8 is_left)
-{
-	// dump calibration data
-	u8* out = read_spi(jc, 0x80, is_left ? 0x12 : 0x1d, 9, is_left);
-	u8 found = 0;
-	for (int i = 0; i < 9; ++i)
-	{
-		if (out[i] != 0xff)
-		{
-			// User calibration data found
-			std::cout << "warning: user calibration data not found" << std::endl;
-			found = 1;
-			break;
-		}
-	}
-	if (!found)
-	{
-		std::cout << "warning: user calibration data not found" << std::endl;
-		out = read_spi(jc, 0x60, is_left ? 0x3d : 0x46, 9, is_left);
-	}
-	stick_cal[is_left ? 4 : 7]  = ((out[7] << 8) & 0xf00) | out[6]; // X Min below center
-	stick_cal[is_left ? 5 : 8]  = ((out[8] << 4) | (out[7] >> 4));  // Y Min below center
-	stick_cal[is_left ? 0 : 9]  = ((out[1] << 8) & 0xf00) | out[0]; // X Max above center
-	stick_cal[is_left ? 1 : 10] = ((out[2] << 4) | (out[1] >> 4));  // Y Max above center
-	stick_cal[is_left ? 2 : 11] = ((out[4] << 8) & 0xf00 | out[3]); // X Center
-	stick_cal[is_left ? 3 : 12] = ((out[5] << 4) | (out[4] >> 4));  // Y Center
-	out = read_spi(jc, 0x60, is_left ? 0x86 : 0x98, 9, is_left);
-	stick_cal[is_left ? 6 : 13] = ((out[4] << 8) & 0xF00 | out[3]);			   // Deadzone
+void get_stick_cal(hid_device* jc, u8 is_left) {
+  // dump calibration data
+  u8* out = read_spi(jc, 0x80, is_left ? 0x12 : 0x1d, 9, is_left);
+  u8 found = 0;
+  for(int i = 0; i < 9; ++i) {
+    if(out[i] != 0xff) {
+      // User calibration data found
+      std::cout << "warning: user calibration data not found" << std::endl;
+      found = 1;
+      break;
+    }
+  }
+  if(!found) {
+    std::cout << "warning: user calibration data not found" << std::endl;
+    out = read_spi(jc, 0x60, is_left ? 0x3d : 0x46, 9, is_left);
+  }
+  stick_cal[is_left ? 4 : 7]  = ((out[7] << 8) & 0xf00) | out[6]; // X Min below center
+  stick_cal[is_left ? 5 : 8]  = ((out[8] << 4) | (out[7] >> 4));  // Y Min below center
+  stick_cal[is_left ? 0 : 9]  = ((out[1] << 8) & 0xf00) | out[0]; // X Max above center
+  stick_cal[is_left ? 1 : 10] = ((out[2] << 4) | (out[1] >> 4));  // Y Max above center
+  stick_cal[is_left ? 2 : 11] = ((out[4] << 8) & 0xf00 | out[3]); // X Center
+  stick_cal[is_left ? 3 : 12] = ((out[5] << 4) | (out[4] >> 4));  // Y Center
+  out = read_spi(jc, 0x60, is_left ? 0x86 : 0x98, 9, is_left);
+  stick_cal[is_left ? 6 : 13] = ((out[4] << 8) & 0xF00 | out[3]);         // Deadzone
 }
 
 void setup_joycon(hid_device *jc, u8 leds, u8 is_left) {
-	u8 send_buf = 0x3f;
-	subcomm(jc, &send_buf, 1, 0x3, 1, is_left);
-	get_stick_cal(jc, is_left);	
-/*	TODO: improve bluetooth pairing
-	send_buf = 0x1;
-	subcomm(jc, &send_buf, 1, 0x1, 1, is_left);
-	send_buf = 0x2;
-	subcomm(jc, &send_buf, 1, 0x1, 1, is_left);
-	send_buf = 0x3;
-	subcomm(jc, &send_buf, 1, 0x1, 1, is_left);*/
-	send_buf = leds;
-	subcomm(jc, &send_buf, 1, 0x30, 1, is_left);
-	send_buf = 0x30;
-	subcomm(jc, &send_buf, 1, 0x3, 1, is_left);
+  u8 send_buf = 0x3f;
+  subcomm(jc, &send_buf, 1, 0x3, 1, is_left);
+  get_stick_cal(jc, is_left);  
+/*  TODO: improve bluetooth pairing
+  send_buf = 0x1;
+  subcomm(jc, &send_buf, 1, 0x1, 1, is_left);
+  send_buf = 0x2;
+  subcomm(jc, &send_buf, 1, 0x1, 1, is_left);
+  send_buf = 0x3;
+  subcomm(jc, &send_buf, 1, 0x1, 1, is_left);*/
+  send_buf = leds;
+  subcomm(jc, &send_buf, 1, 0x30, 1, is_left);
+  send_buf = 0x30;
+  subcomm(jc, &send_buf, 1, 0x3, 1, is_left);
 }
 
 void initialize_left_joycon() {
@@ -425,42 +418,39 @@ void disconnect_exit() {
 }
 
 void process_stick(bool is_left, uint8_t a, uint8_t b, uint8_t c) {
-	u16 raw[] = { (uint16_t)(a | ((b & 0xf) << 8)), \
-						 (uint16_t)((b >> 4) | (c << 4)) };
-	float s[] = { 0, 0 };
-	u8 offset = is_left ? 0 : 7;
-	for (u8 i = 0; i < 2; ++i)
-	{
-		s[i] = (raw[i] - stick_cal[i + 2 + offset]);
-		if (abs(s[i]) < stick_cal[6 + offset]) s[i] = 0; // inside deadzone
-		else if (s[i] > 0)						// axis is above center
-		{
-			s[i] /= stick_cal[i + offset];
-		}
-		else									// axis is below center
-		{
-			s[i] /= stick_cal[i + 4 + offset];
-		}
-		if (s[i] > 1)  s[i] = 1;
-		if (s[i] < -1) s[i] = -1;
-		s[i] *= (XBOX_ANALOG_MAX);
-	}
+  u16 raw[] = { (uint16_t)(a | ((b & 0xf) << 8)), \
+             (uint16_t)((b >> 4) | (c << 4)) };
+  float s[] = { 0, 0 };
+  u8 offset = is_left ? 0 : 7;
+  for(u8 i = 0; i < 2; ++i) {
+    s[i] = (raw[i] - stick_cal[i + 2 + offset]);
+    if(abs(s[i]) < stick_cal[6 + offset]) s[i] = 0; // inside deadzone
+    else if(s[i] > 0)            // axis is above center{
+      s[i] /= stick_cal[i + offset];
+    }
+    else                  // axis is below center{
+      s[i] /= stick_cal[i + 4 + offset];
+    }
+    if(s[i] > 1)  s[i] = 1;
+    if(s[i] < -1) s[i] = -1;
+    s[i] *= (XBOX_ANALOG_MAX);
+  }
 
-	if (is_left) {
-		report.sThumbLX = (SHORT)s[0];
-		report.sThumbLY = (SHORT)s[1];
-	}
-	else {
-		report.sThumbRX = (SHORT)s[0];
-		report.sThumbRY = (SHORT)s[1];
-	}
+  if(is_left) {
+    report.sThumbLX = (SHORT)s[0];
+    report.sThumbLY = (SHORT)s[1];
+  }
+  else {
+    report.sThumbRX = (SHORT)s[0];
+    report.sThumbRY = (SHORT)s[1];
+  }
 }
 
 void process_button(JOYCON_REGION region, JOYCON_BUTTON button) {
   if(!((region == LEFT_ANALOG && button == L_ANALOG_NONE) || (region == RIGHT_ANALOG && button == R_ANALOG_NONE)))
   std::cout << joycon_button_to_string(region, button) << " ";
   auto got = button_mappings.find(button);
-  if (got != button_mappings.end()) {
+  if(got != button_mappings.end()) {
     XUSB_BUTTON target = got->second;
     switch (region) {
       case LEFT_DPAD:
@@ -678,16 +668,16 @@ void process_left_joycon() {
   u8 offset = 0;
   u8 shift = 0;
   u8 offset2 = 0;
-  if (data[0] == 0x30 || data[0] == 0x21) {
-	  // 0x30 input reports order the button status data differently
-	  // this approach is ugly, but doesn't require changing the enum
-	  offset = 2;
-	  offset2 = 3;
-	  shift = 1;
-	  process_stick(true, data[6], data[7], data[8]);
+  if(data[0] == 0x30 || data[0] == 0x21) {
+    // 0x30 input reports order the button status data differently
+    // this approach is ugly, but doesn't require changing the enum
+    offset = 2;
+    offset2 = 3;
+    shift = 1;
+    process_stick(true, data[6], data[7], data[8]);
   }
   else {
-	  process_buttons(LEFT_ANALOG, (JOYCON_BUTTON)data[3]);
+    process_buttons(LEFT_ANALOG, (JOYCON_BUTTON)data[3]);
   }
   region_part(data[1 + offset*2]<<shift, LEFT_DPAD, L_DPAD_UP);
   region_part(data[1 + offset*2]<<shift, LEFT_DPAD, L_DPAD_DOWN);
@@ -709,13 +699,13 @@ void process_right_joycon() {
   u8 offset = 0;
   u8 shift = 0;
   u8 offset2 = 0;
-  if (data[0] == 0x30 || data[0] == 0x21) {
-	  // 0x30 input reports order the button status data differently
-	  // this approach is ugly, but doesn't require changing the enum
-	  offset = 2;
-	  offset2 = 1;
-	  shift = 1;
-	  process_stick(false, data[9], data[10], data[11]);
+  if(data[0] == 0x30 || data[0] == 0x21) {
+    // 0x30 input reports order the button status data differently
+    // this approach is ugly, but doesn't require changing the enum
+    offset = 2;
+    offset2 = 1;
+    shift = 1;
+    process_stick(false, data[9], data[10], data[11]);
   } else process_buttons(RIGHT_ANALOG, (JOYCON_BUTTON)data[3]);
   region_part(data[1 + offset]>>(shift*3), RIGHT_BUTTONS, R_BUT_A);
   region_part(data[1 + offset], RIGHT_BUTTONS, R_BUT_B);
@@ -731,10 +721,9 @@ void process_right_joycon() {
   report.wButtons = left_buttons | right_buttons;
 }
 
-void joycon_cleanup(hid_device *jc, u8 is_left)
-{
-	u8 send_buf = 0x3f;
-	subcomm(jc, &send_buf, 1, 0x3, 1, is_left);
+void joycon_cleanup(hid_device *jc, u8 is_left) {
+  u8 send_buf = 0x3f;
+  subcomm(jc, &send_buf, 1, 0x3, 1, is_left);
 }
 
 DWORD WINAPI left_joycon_thread(__in LPVOID lpParameter) {
