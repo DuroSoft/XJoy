940de57bc9dec71e8dcb6a47ca48f930a3bfec62
NyezE <88356594+NyezE@users.noreply.github.com>
2021-08-03 08:10:39 +0530

    XJoy Remap
    I want the ABXY to be ABXY...not BAYX

--- XJoy/XJoy.cpp
+++ XJoy/XJoy.cpp
@@ -1087,16 +1087,16 @@ void process_button(Xbox* xbox, JOYCON_REGION region, JOYCON_BUTTON button) {
       break;
     case RIGHT_BUTTONS:
       switch(button) {
-        case R_BUT_A:
+        case R_BUT_B:
 			xbox->right_buttons = xbox->right_buttons | XUSB_GAMEPAD_B;
           break;
-        case R_BUT_B:
+        case R_BUT_A:
 			xbox->right_buttons = xbox->right_buttons | XUSB_GAMEPAD_A;
           break;
-        case R_BUT_X:
+        case R_BUT_Y:
 			xbox->right_buttons = xbox->right_buttons | XUSB_GAMEPAD_Y;
           break;
-        case R_BUT_Y:
+        case R_BUT_X:
 			xbox->right_buttons = xbox->right_buttons | XUSB_GAMEPAD_X;
           break;
         case R_BUT_SL:
